8edcf4e56a6673034e339ac80f79045a1695878d
Aaron Murray <plugins@omv-extras.org>
2022-02-27 08:48:27 -0600

    Add new policies for issue #41

--- var/www/openmediavault/js/omv/module/admin/storage/unionfilesystems/MergerfsCreatePolicyStore.js
+++ var/www/openmediavault/js/omv/module/admin/storage/unionfilesystems/MergerfsCreatePolicyStore.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2014-2020 OpenMediaVault Plugin Developers
+ * Copyright (C) 2014-2022 OpenMediaVault Plugin Developers
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -39,7 +39,12 @@ Ext.define('OMV.module.admin.storage.unionfilesystems.MergerfsCreatePolicyStore'
         ['lfs', _('Least free space.')],
         ['lus', _('Least used space.')],
         ['mfs', _('Most free space.')],
+        ['msplfs', _('Most shared path - least free space')],
+        ['msplus', _('Most shared path - least used space')],
+        ['mspmfs', _('Most shared path - most free space')],
+        ['msppfrd', _('Most shared path - percentage free random distribution')],
         ['newest', _('Newest file.')],
+        ['pfrd', _('Percentage free random distribution')],
         ['rand', _('Random.')]
     ]
 });
